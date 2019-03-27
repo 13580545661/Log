217df7d7cb25d2f03017594209fa92dd9efb41c6
hipsnafoha <hipsnafoha@outlook.com>
2019-03-27 19:15:47 +0800

    Add Toolbar and Bottom Navigation (WIP).

---
+++ app/src/main/java/com/weacadt/log/activity/BottomNavigationActivity.java
@@ -0,0 +1,17 @@
+package com.weacadt.log.activity;
+
+
+import android.os.Bundle;
+
+import com.weacadt.log.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BottomNavigationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_navigation);
+    }
+}
---
+++ app/src/main/java/com/weacadt/log/activity/MainActivity.java
@@ -0,0 +1,19 @@
+package com.weacadt.log.activity;
+
+import android.os.Bundle;
+
+import com.weacadt.log.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+
+}
+
+
