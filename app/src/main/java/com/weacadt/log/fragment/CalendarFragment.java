a6713c29170aee19329f2c1482ee808ea8bd20c0
lwh <liangwenhao9@outlook.com>
2019-04-10 21:13:27 +0800

    Add calendar

--- app/src/main/java/com/weacadt/log/fragment/CalendarFragment.java
+++ app/src/main/java/com/weacadt/log/fragment/CalendarFragment.java
@@ -4,18 +4,36 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 import com.weacadt.log.R;
+
+import java.util.Calendar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class CalendarFragment extends Fragment {
+    CalendarView calendarView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_calendar, null);
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        calendarView = getActivity().findViewById(R.id.calendar);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                Toast.makeText(getContext(), "你选择了"+year+"年"+month+"月"+dayOfMonth+"日", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
